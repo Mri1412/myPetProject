f02f03ae5b71b1f5d8cd20fe473cd23edae31ce5
Mri1412 <marianneknst@gmail.com>
2019-12-27 18:53:50 +0100

    Implement workout and exercise

---
+++ app/src/main/java/com/example/myapplication/logic/exercise/ExerciseImpl.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.logic.exercise;
+
+public class ExerciseImpl implements Exercise {
+
+    //Properties
+    String exerciseName;
+
+    //Constructor
+    public ExerciseImpl(){
+    }
+
+    public ExerciseImpl(String name){
+        exerciseName = name;
+    }
+
+    // Interface methods
+    @Override
+    public String getName() {
+        return exerciseName;
+    }
+
+}
